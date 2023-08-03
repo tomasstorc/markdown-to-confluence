119bb87fd7c351b86c5ee9383afae45b28637c39
Tomas Storc <tomas.storc@gmail.com>
2023-08-03 12:56:55 +0200

    add file option

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ const convert2html = (text) => {
   return converter.makeHtml(text);
 };
 try {
-  console.log(fs.readdirSync("."));
-  let content = core.getInput("markdown");
-  console.log(content);
-  let converted = convert2html(content);
-  console.log(converted);
+  if (core.getInput("markdown")) {
+    console.log("using text as input");
+    let content = core.getInput("markdown");
+    console.log(content);
+    let converted = convert2html(content);
+    console.log(converted);
+  }
+  if (core.getInput("filename")) {
+    console.log("using file as input");
+    let filename = core.getInput("filename");
+    let converted = convert2html(fs.readFileSync(filename).toString());
+    console.log(converted);
+  }
 } catch (e) {
   core.setFailed(e.message);
 }
