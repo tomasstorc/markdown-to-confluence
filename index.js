de5b4ce39ff810a6cc9bac4e282db86c1c203b73
Tomas Storc <tomas.storc@gmail.com>
2023-08-03 11:00:20 +0200

    convert

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const github = require("@actions/github");
 const fs = require("fs");
 const showdown = require("showdown");
 
+const convert2html = (text) => {
+  let converter = new showdown.Converter();
+  return converter.convert2html(text);
+};
 try {
   core.setOutput("converted", fs.readFileSync("README.md"));
-  console.log(process.env.TEST);
-  console.log(fs.readFileSync("README.md").toString());
+  let fileText = fs.readFileSync("README.md").toString();
+  let converted = convert2html(fileText);
+  console.log(converted);
 } catch (e) {
   core.setFailed(e.message);
 }
