4ea62d8f7afce5cf108956ffba614790eb75a5aa
Tomas Storc <tomas.storc@gmail.com>
2023-08-03 10:44:02 +0200

    create action

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+const showdown = require("showdown");
+
+try {
+  fs.readdir(testFolder, (err, files) => {
+    files.forEach((file) => {
+      console.log(file);
+      core.setOutput("converted", fs.readFileSync(file));
+    });
+  });
+} catch (e) {
+  core.setFailed(e.message);
+}
