a4b017fa1e58846d02af62a862e67f7f24fe6969
Tomas Storc <tomas.storc@gmail.com>
2023-08-07 12:56:55 +0200

    reduced size to half

---
+++ src/api.ts
@@ -0,0 +1,94 @@
+import {getInput, setFailed} from '@actions/core'
+import {handleAuth, isCloud, URL} from './utils'
+
+export const publishContent = (content: string | undefined) => {
+  const basicauth = handleAuth()
+  const payload = {
+    type: 'page',
+    title: getInput('title'),
+    space: {key: getInput('spacekey')},
+    body: {
+      storage: {
+        value: content,
+        representation: 'storage'
+      }
+    }
+  }
+  fetch(URL, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basicauth}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  })
+    .then((res: any) => {
+      return res
+    })
+    .then(() => {
+      console.log('successfully published')
+    })
+}
+
+export const updateContent = async (
+  content: string | undefined,
+  id: string
+) => {
+  const basicauth = handleAuth()
+  const newVersion = await handleVersion(id)
+  const payload = {
+    id,
+    type: 'page',
+    title: getInput('title'),
+    space: {key: getInput('spacekey')},
+    body: {
+      storage: {
+        value: content,
+        representation: 'storage'
+      }
+    },
+    version: {
+      number: newVersion
+    }
+  }
+  fetch(`${URL}/${id}`, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Basic ${basicauth}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  })
+    .then((res: any) => {
+      if (res.status === 409) return setFailed('this version already exists')
+      return res
+    })
+    .then(() => {
+      console.log('successfully updated')
+    })
+}
+
+export const findExisting = async () => {
+  const basicauth = handleAuth()
+  const res = await fetch(
+    `${URL}?spaceKey=${getInput('spacekey')}&title=${getInput('title')}`,
+    {
+      headers: {
+        Authorization: `Basic ${basicauth}`
+      }
+    }
+  )
+  const data: any = await res.json()
+  return data.results[0]?.id ? data.results[0].id : ''
+}
+
+export const handleVersion = async (id: string) => {
+  const basicauth = handleAuth()
+  const res = await fetch(`${URL}/${id}`, {
+    headers: {
+      Authorization: `Basic ${basicauth}`
+    }
+  })
+  const data: any = await res.json()
+  return +data.version.number + 1
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
-import * as core from '@actions/core'
+import {getInput, setFailed} from '@actions/core'
+import {readFileSync} from 'fs'
+import {Converter} from 'showdown'
 
 export const isCloud = (url: string): boolean => {
   const suffix = url.slice(-2)
@@ -6,23 +8,45 @@ export const isCloud = (url: string): boolean => {
 }
 
 export const handleAuth = () => {
-  return core.getInput('basicauth')
-    ? core.getInput('basicauth')
-    : Buffer.from(
-        `${core.getInput('cnfluser')}:${core.getInput('apikey')}`
-      ).toString('base64')
+  return getInput('basicauth')
+    ? getInput('basicauth')
+    : Buffer.from(`${getInput('cnfluser')}:${getInput('apikey')}`).toString(
+        'base64'
+      )
 }
 
-export const handleVersion = async (id: string) => {
-  const basicauth = handleAuth()
-  const res = await fetch(
-    `${core.getInput('cnflurl')}/wiki/rest/api/content/${id}`,
-    {
-      headers: {
-        Authorization: `Basic ${basicauth}`
-      }
-    }
-  )
-  const data: any = await res.json()
-  return +data.version.number + 1
+export const checkInputs = () => {
+  !getInput('spacekey') && setFailed('Confluence space key is missing, exiting')
+  !getInput('cnflurl') && setFailed('Confluence URL is missing, exiting')
+  !getInput('apikey') && setFailed('Confluence API key is missing, exiting')
+  !getInput('cnfluser') && setFailed('Confluence user is missing, exiting')
+  !getInput('title') && setFailed('Page title is missing, exiting')
+  !getInput('filename') &&
+    !getInput('markdown') &&
+    setFailed('Markdown string or markdown file are missing, exiting')
 }
+
+const convert2html = (text: string): string => {
+  let converter = new Converter()
+  return converter.makeHtml(text)
+}
+export const convertFn = () => {
+  try {
+    if (getInput('markdown')) {
+      console.log('using text as input')
+      let content = getInput('markdown')
+      return convert2html(content)
+    }
+    if (getInput('filename')) {
+      console.log('using file as input')
+      let filename = getInput('filename')
+      return convert2html(readFileSync(filename).toString())
+    }
+  } catch (e: any) {
+    setFailed(e.message)
+  }
+}
+
+export const URL = `${getInput('cnflurl')}${
+  isCloud(getInput('cnflurl')) && 'wiki'
+}/rest/api/content`
